10d36428a313aa32b9063a0c9b5e03f2705529c2
jvalero <jvalero84@gmail.com>
2022-11-24 22:29:11 +0100

    QuoteModel entity class and start of IngestorInterface

---
+++ src/QuoteEngine/QuoteModel.py
@@ -0,0 +1,13 @@
+
+"""A module containing a QuoteModel class."""
+class QuoteModel:
+    """A class representing a Quote made up of a quote body and the author."""
+
+    def __init__(self, body:str, author:str):
+        """Construct a QuoteModel object initializing two attributes."""
+        self.body = body
+        self.author = author
+    
+    def __str__(self):
+        """Print the string representation of this object as "body" - author."""
+        return f'"{self.body}" - {self.author}'
